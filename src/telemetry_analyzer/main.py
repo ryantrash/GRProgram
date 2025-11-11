a90d8c3a603490dd5ba51d42da7064d2052d0d4a
ryantrash <ryandherber@gmail.com>
2025-11-11 16:25:05 -0600

    added id to getfastestlaps function

--- src/telemetry_analyzer/main.py
+++ src/telemetry_analyzer/main.py
@@ -12,19 +12,18 @@ engine = create_engine(
 )
 
 def getFastestLaps(df):
-    laps = {}
+    laps = []
     for i in range(1, len(df.columns)):
-        colname = df.columns[i] 
+        lap = df.columns[i] 
         coldata = df.iloc[:, i] 
-
         coldata.sort_values()
         fastest = -1.0
-        for time in coldata:
-            if(time > 0.0):
-                fastest = time 
-                break
-            
-        laps[colname] = fastest
+        for j in range(0, len(coldata)):
+            time = coldata.iloc[j].item()
+            car_id = coldata.index[i].item()
+            if time > 0.0:
+                laps.append([lap, car_id, time])
+                break 
     return laps
 
 lap_df = pd.read_csv("./COTA/race_1/laptimes.csv")
