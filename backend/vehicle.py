b09c8ec8aa210e9c14eeb9d89247e69d33f069ef
ryantrash <ryandherber@gmail.com>
2025-11-14 23:01:52 -0600

    to_seconds converter for vehicle data

--- backend/vehicle.py
+++ backend/vehicle.py
@@ -17,3 +17,22 @@ class Vehicle:
                 self.speed.append(tEntry)
             elif tName == "pbrake_f":
                 self.brake.append(tEntry)
+    
+    def toSeconds(self, arr): 
+        by_seconds = [] 
+        count = 1
+        vals = [arr[0][0]]
+        current_second = arr[0][1][14:19]
+        
+        for i in range(1, len(arr)):
+            val = arr[i][1]
+            if current_second == val[14:19]: 
+                vals.append(arr[i][0])
+            else: 
+                cur = arr[i][1] 
+                by_seconds.append(sum(vals)/len(vals))
+                vals = [arr[i][0]] 
+        
+        by_seconds.append(sum(vals)/len(vals))
+        
+        return by_seconds       
